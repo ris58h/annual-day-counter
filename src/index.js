cbaded6aae23188a25bb0c4ff2be6a8af5712b31
Ilya Rodionov <ris58h@gmail.com>
2022-09-05 18:17:32 +0300

    Initial version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,178 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Day({value, selected, onClick}) {
+    return (
+        <button className={'day' + (selected ? ' selected' : '')} onClick={onClick}>
+            {value}
+        </button>
+    );
+}
+
+function Month({year, month, selectedDays, onDayClick}) {
+    function renderDay(day, key) {
+        const selected = selectedDays?.has(day);
+
+        return (
+            <Day
+                key={key}
+                value={day}
+                selected={selected}
+                onClick={() => onDayClick(day)}
+            />
+        );
+    }
+
+    function renderEmptyDay(key) {
+        return (
+            <Day
+                key={key}
+                value={''}
+            />
+        );
+    }
+
+    function renderWeek(days, key) {
+        return (
+            <div className='week' key={key}>
+                {days}
+            </div>
+        );
+    }
+
+    const daysBefore = new Date(year, month).getDay() - 1;
+    const daysInMonth = new Date(year, month, 0).getDate();
+    const weeksInMonth = Math.ceil((daysBefore + daysInMonth) / 7);
+    const weeks = [];
+    let days = [];
+    for (let i = 1; i <= weeksInMonth * 7; i++) {
+        const day = (i <= daysBefore || i > daysBefore + daysInMonth)
+            ? renderEmptyDay(i)
+            : renderDay(i - daysBefore, i);
+        days.push(day);
+        if (days.length === 7) {
+            weeks.push(renderWeek(days, i));
+            days = [];
+        }
+    }
+
+    return (
+        <div className='month'>
+            {weeks}
+        </div>
+    );
+}
+
+const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function MonthSwitcher({year, month, onPrevMonth, onNextMonth}) {
+    return (
+        <div className='month-switcher'>
+            <button onClick={onPrevMonth}>{'<'}</button>
+            <span>{MONTH_NAMES[month] + ' ' + year}</span>
+            <button onClick={onNextMonth}>{'>'}</button>
+            
+        </div>
+    );
+}
+
+function YearSwitcher({year, onPrevYear, onNextYear, selectedDays}) {
+    const selectedCount = countSelectedDays();
+    const unselectedCount = daysInYear(year) - selectedCount;
+
+    return (
+        <div className='year-switcher'>
+            <button onClick={onPrevYear}>{'<'}</button>
+            <span>{year}</span>
+            <button onClick={onNextYear}>{'>'}</button>
+            <span>{selectedCount}</span>
+            <span>{unselectedCount}</span>
+        </div>
+    );
+
+    function countSelectedDays() {
+        if (!selectedDays) {
+            return 0;
+        }
+        let res = 0;
+        for (let selectedMonthDays of selectedDays.values()) {
+            res += selectedMonthDays.size;
+        }
+        return res;
+    }
+}
+
+function Calendar() {
+    const now = new Date();
+    const [year, setYear] = useState(now.getFullYear());
+    const [month, setMonth] = useState(now.getMonth());
+    const [selectedDays, setSelectedDays] = useState(new Map());
+
+    return (
+        <div className='calendar'>
+            <YearSwitcher
+                year={year}
+                onPrevYear={() => setYear(year - 1)}
+                onNextYear={() => setYear(year + 1)}
+                selectedDays={selectedDays.get(year)}
+            />
+            <MonthSwitcher
+                year={year}
+                month={month}
+                onPrevMonth={handlePrevMonth}
+                onNextMonth={handleNextMonth}
+            />
+            <Month
+                year={year}
+                month={month}
+                selectedDays={selectedDays.get(year)?.get(month)}
+                onDayClick={handleDayClick}
+            />
+        </div>
+    );
+
+    function handleDayClick(day) {
+        const newSelectedDays = new Map(selectedDays);
+
+        const yearSelectedDays = new Map(newSelectedDays.get(year));
+        newSelectedDays.set(year, yearSelectedDays);
+
+        const monthSelectedDays = new Set(yearSelectedDays.get(month));
+        yearSelectedDays.set(month, monthSelectedDays);
+
+        if (monthSelectedDays.has(day)) {
+            monthSelectedDays.delete(day);
+        } else {
+            monthSelectedDays.add(day);
+        }
+
+        setSelectedDays(newSelectedDays);
+    }
+
+    function handlePrevMonth() {
+        if (month === 0) {
+            setYear(year - 1);
+            setMonth(11);
+        } else {
+            setMonth(month - 1);
+        }
+    }
+
+    function handleNextMonth() {
+        if (month === 11) {
+            setYear(year + 1);
+            setMonth(0);
+        } else {
+            setMonth(month + 1);
+        }
+    }
+}
+
+function daysInYear(year) {
+    return ((year % 4 === 0 && year % 100 > 0) || year %400 === 0) ? 366 : 365;
+}
+// ========================================
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Calendar />);
